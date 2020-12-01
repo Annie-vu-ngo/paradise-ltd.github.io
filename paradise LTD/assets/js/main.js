6106a7786306dd0ade73f6ba6cd296d4f3aec4d0
Annie-vu-ngo <ango0008@student.monash.edu>
2020-12-01 19:57:41 +1100

    image-nets

---
+++ paradise LTD/assets/js/main.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+
+$('.btn-Nos').click(function(){
+	$('.item').addClass('hide');
+	$('.item.Nos').removeClass('hide');
+});
+
+$('.btn-escap').click(function(){
+	$('.item').addClass('hide');
+	$('.item.escap').removeClass('hide');
+});
+
+$('.btn-consump').click(function(){
+	$('.item').addClass('hide');
+	$('.item.consump').removeClass('hide');
+});
+
+$('.btn-pop').click(function(){
+	$('.item').addClass('hide');
+	$('.item.pop').removeClass('hide');
+});
+
+});
+
